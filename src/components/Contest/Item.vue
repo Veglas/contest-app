97384c4e8c1d2c7d3c763cb84e71302888555bcb
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2018-02-24 03:15:15 +0200

    fix image at Item page

--- src/components/Contest/Item.vue
+++ src/components/Contest/Item.vue
@@ -4,20 +4,22 @@
       <v-flex xs12 sm8 offset-sm2>
 
         <v-card>
-          <v-card-media height="480" :src="item.imageUrl">
-
-            <v-spacer/>
-
-            <v-btn fab v-if="currentUserId === 'toxjaps6DjgDKrju6hf6Iq2e9FR2'">
-              <v-icon>security</v-icon>
-            </v-btn>
-
-            <v-btn fab v-if="userIsCreator">
-              <v-icon>mode_edit</v-icon>
-            </v-btn>
-
-          </v-card-media>
           <v-card-text>
+            <div class="buttons-group">
+
+              <v-spacer/>
+
+              <v-btn fab v-if="currentUserId === 'toxjaps6DjgDKrju6hf6Iq2e9FR2'" color="warning">
+                <v-icon>security</v-icon>
+              </v-btn>
+
+              <v-btn fab v-if="userIsCreator">
+                <v-icon>mode_edit</v-icon>
+              </v-btn>
+
+            </div>
+            <img :src="item.imageUrl" style="max-width: 100%">
+            <br>
             ID билета: <b>{{ item.id }}</b>
             <br>
             ID участника: <b>{{ item.creatorId }}</b>
@@ -58,4 +60,9 @@
 </script>
 
 <style scoped>
+  .buttons-group {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
 </style>
