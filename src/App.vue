c807050559234931a5c0d89c53c6dca2d0efcb9c
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2018-02-26 01:47:15 +0200

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,167 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+
+    <v-toolbar dark class="black">
+      <v-toolbar-items>
+        <v-btn flat :href="veglasBtn.url">
+          <v-icon style="color: #00b535">{{ veglasBtn.icon }}</v-icon>
+          <v-toolbar-title style="color: #d89924">{{ veglasBtn.title }}</v-toolbar-title>
+        </v-btn>
+      </v-toolbar-items>
+
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat :to="homeBtn.url" exact>
+          <v-icon>{{ homeBtn.icon }}</v-icon>
+          <v-toolbar-title>{{ homeBtn.title }}</v-toolbar-title>
+        </v-btn>
+
+        <v-btn flat :to="contestBtn.url">
+          <v-icon left>{{ contestBtn.icon }}</v-icon>
+          <span>{{ contestBtn.title }}</span>
+        </v-btn>
+      </v-toolbar-items>
+
+      <v-spacer/>
+
+      <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+
+      <v-toolbar-items class="hidden-xs-only">
+
+        <v-btn flat v-if="currentUserId === 'toxjaps6DjgDKrju6hf6Iq2e9FR2'" :to="adminBtn.url">
+          <v-icon left>{{ adminBtn.icon }}</v-icon>
+          <span>{{ adminBtn.title }}</span>
+        </v-btn>
+
+        <v-btn flat v-if="userIsAuthenticated" :to="profileBtn.url">
+          <v-icon left>{{ profileBtn.icon }}</v-icon>
+          <span>{{ profileBtn.title }}</span>
+        </v-btn>
+
+        <v-btn flat v-if="!userIsAuthenticated" :to="loginBtn.url">
+          <v-icon left>{{ loginBtn.icon }}</v-icon>
+          <span>{{ loginBtn.title }}</span>
+        </v-btn>
+
+        <v-btn flat v-if="!userIsAuthenticated" :to="registerBtn.url">
+          <v-icon left>{{ registerBtn.icon }}</v-icon>
+          <span>{{ registerBtn.title }}</span>
+        </v-btn>
+
+      </v-toolbar-items>
     </v-toolbar>
+
     <v-content>
       <router-view/>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
+
+    <v-card flat tile dark class="grey darken-4">
+      <v-card-text>
+        <v-container>
+          <v-layout row wrap>
+            <v-flex xs12>
+              <span>&copy; {{ new Date().getFullYear() }} Все права защищены | 18+ | Веглас Casino Streams</span>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+    </v-card>
+
+    <v-navigation-drawer fixed temporary right v-model="sideNav">
+      <v-list dense class="pt-0">
+
+        <v-list-tile :to="veglasBtn.url">
           <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
+            <v-icon>{{ veglasBtn.icon }}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ veglasBtn.title }}</v-list-tile-title>
+          </v-list-tile-content>
         </v-list-tile>
+
+        <v-list-tile :to="homeBtn.url">
+          <v-list-tile-action>
+            <v-icon>{{ homeBtn.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ homeBtn.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile :to="contestBtn.url">
+          <v-list-tile-action>
+            <v-icon>{{ contestBtn.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ contestBtn.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile v-if="currentUserId === 'toxjaps6DjgDKrju6hf6Iq2e9FR2'" :to="adminBtn.url">
+          <v-list-tile-action>
+            <v-icon>{{ adminBtn.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ adminBtn.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile v-if="userIsAuthenticated" :to="profileBtn.url">
+          <v-list-tile-action>
+            <v-icon>{{ profileBtn.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ profileBtn.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile v-if="!userIsAuthenticated" :to="loginBtn.url">
+          <v-list-tile-action>
+            <v-icon>{{ loginBtn.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ loginBtn.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile v-if="!userIsAuthenticated" :to="registerBtn.url">
+          <v-list-tile-action>
+            <v-icon>{{ registerBtn.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ registerBtn.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
       </v-list>
     </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-      <Network/>
-    </v-footer>
+
   </v-app>
 </template>
 
 <script>
-  import Network from './components/Network.vue'
-
   export default {
-    components: {Network},
     data () {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [{
-          icon: 'bubble_chart',
-          title: 'Inspire'
-        }],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+        sideNav: null,
+        veglasBtn: {title: 'Веглас', icon: 'mdi-bitcoin', url: 'https://veglas.org'},
+        homeBtn: {title: 'Лотерея', icon: 'mdi-crown', url: '/'},
+        profileBtn: {title: 'Профиль', icon: 'mdi-account', url: '/user/profile'},
+        adminBtn: {title: 'Админ', icon: 'mdi-security', url: '/admin'},
+        contestBtn: {title: 'Все билеты', icon: 'mdi-clover', url: '/contest'},
+        loginBtn: {title: 'Вход', icon: 'mdi-key', url: '/user/login'},
+        registerBtn: {title: 'Регистрация', icon: 'mdi-account-plus', url: '/user/register'}
       }
     },
-    name: 'App'
+    computed: {
+      userIsAuthenticated () {
+        return this.$store.getters.user !== null && this.$store.getters.user !== undefined
+      },
+      currentUserId () {
+        if (!this.userIsAuthenticated) {
+          return false
+        }
+        return this.$store.getters.user.id
+      }
+    }
   }
 </script>
