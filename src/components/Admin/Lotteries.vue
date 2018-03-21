7a3d7c3e6708a4f6fe004aa0347af79b542b6a81
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2018-03-21 16:55:15 +0200

    upd yandex metrika informer

--- src/components/Admin/Lotteries.vue
+++ src/components/Admin/Lotteries.vue
@@ -22,11 +22,6 @@
 
               <v-flex xs12>
                 <h1>Лотереи</h1>
-
-                <a href="https://metrika.yandex.ru/stat/?id=48127781&amp;from=informer"
-                target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/48127781/1_0_99FF99FF_99FF99FF_0_pageviews"
-                style="width:80px; height:15px; border:0;" alt="яндекс.метрика" title="яндекс.метрика: данные за сегодня (просмотры)" /></a>
-
               </v-flex>
 
             </v-layout>
