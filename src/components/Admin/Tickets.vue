c8f0adc7b21d7c9bdb5da9ca35bafa289c6aba65
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2018-03-25 17:21:29 +0300

    rework Admin/Tickets

--- src/components/Admin/Tickets.vue
+++ src/components/Admin/Tickets.vue
@@ -37,64 +37,6 @@
 
               <v-flex xs12 class="text-xs-center">
                 <h2>
-                  <v-chip small color="success white--text">
-                    <v-icon>mdi-eye</v-icon>
-                  </v-chip>
-                  <span>Билеты, прошедшие модерацию</span>
-                </h2>
-              </v-flex>
-
-              <v-flex xs12 md3>
-                <div><b>Дата</b></div>
-                <div v-for="i in items" :key="i.id" v-if="i.isModerated">{{ i.date | date }}</div>
-              </v-flex>
-
-              <v-flex xs12 md3>
-                <div><b>ID билета</b></div>
-                <div v-for="i in items" :key="i.id" v-if="i.isModerated">
-                  <a @click="onLoadItem(i.id)">{{ i.id }}</a>
-                </div>
-              </v-flex>
-
-              <v-flex xs12 md4>
-                <div><b>ID юзера</b></div>
-                <div v-for="i in items" :key="i.id" v-if="i.isModerated">
-                  <a @click="onLoadUser(i.creatorId)">{{ i.creatorId }}</a>
-                </div>
-              </v-flex>
-
-              <v-flex xs12 md2>
-                <div><b>Победитель</b></div>
-                <div v-for="i in items" :key="i.id" v-if="i.isModerated">
-
-                  <span>
-
-                    <v-tooltip top v-if="i.isWinnerWeek" color="light-green darken-1" open-delay="0">
-                      <v-icon style="font-size: 20px" color="light-green darken-1" slot="activator">mdi-crown</v-icon>
-                      <span>{{ i.isWinnerWeek }}</span>
-                    </v-tooltip>
-                    <v-icon v-else style="font-size: 20px" color="grey lighten-2">mdi-crown</v-icon>
-
-                    <v-tooltip top v-if="i.isWinnerMonth" color="green darken-1" open-delay="0">
-                      <v-icon style="font-size: 20px" color="green darken-1" slot="activator">mdi-crown</v-icon>
-                      <span>{{ i.isWinnerMonth }}</span>
-                    </v-tooltip>
-                    <v-icon v-else style="font-size: 20px" color="grey lighten-2">mdi-crown</v-icon>
-
-                    <v-tooltip top v-if="i.isWinnerContest" color="teal darken-1" open-delay="0">
-                      <v-icon style="font-size: 20px" color="teal darken-1" slot="activator">mdi-crown</v-icon>
-                      <span>{{ i.isWinnerContest }}</span>
-                    </v-tooltip>
-                    <v-icon v-else style="font-size: 20px" color="grey lighten-2">mdi-crown</v-icon>
-
-                  </span>
-                  <br>
-
-                </div>
-              </v-flex>
-
-              <v-flex xs12 class="text-xs-center">
-                <h2>
                   <v-chip small color="warning white--text">
                     <v-icon>mdi-eye-off</v-icon>
                   </v-chip>
@@ -124,6 +66,64 @@
               <v-flex xs12 md2>
                 <div><b>Победитель</b></div>
                 <div v-for="i in items" :key="i.id" v-if="!i.isModerated">
+
+                  <span>
+
+                    <v-tooltip top v-if="i.isWinnerWeek" color="light-green darken-1" open-delay="0">
+                      <v-icon style="font-size: 20px" color="light-green darken-1" slot="activator">mdi-crown</v-icon>
+                      <span>{{ i.isWinnerWeek }}</span>
+                    </v-tooltip>
+                    <v-icon v-else style="font-size: 20px" color="grey lighten-2">mdi-crown</v-icon>
+
+                    <v-tooltip top v-if="i.isWinnerMonth" color="green darken-1" open-delay="0">
+                      <v-icon style="font-size: 20px" color="green darken-1" slot="activator">mdi-crown</v-icon>
+                      <span>{{ i.isWinnerMonth }}</span>
+                    </v-tooltip>
+                    <v-icon v-else style="font-size: 20px" color="grey lighten-2">mdi-crown</v-icon>
+
+                    <v-tooltip top v-if="i.isWinnerContest" color="teal darken-1" open-delay="0">
+                      <v-icon style="font-size: 20px" color="teal darken-1" slot="activator">mdi-crown</v-icon>
+                      <span>{{ i.isWinnerContest }}</span>
+                    </v-tooltip>
+                    <v-icon v-else style="font-size: 20px" color="grey lighten-2">mdi-crown</v-icon>
+
+                  </span>
+                  <br>
+
+                </div>
+              </v-flex>
+              
+              <v-flex xs12 class="text-xs-center">
+                <h2>
+                  <v-chip small color="success white--text">
+                    <v-icon>mdi-eye</v-icon>
+                  </v-chip>
+                  <span>Билеты, прошедшие модерацию</span>
+                </h2>
+              </v-flex>
+
+              <v-flex xs12 md3>
+                <div><b>Дата</b></div>
+                <div v-for="i in items" :key="i.id" v-if="i.isModerated">{{ i.date | date }}</div>
+              </v-flex>
+
+              <v-flex xs12 md3>
+                <div><b>ID билета</b></div>
+                <div v-for="i in items" :key="i.id" v-if="i.isModerated">
+                  <a @click="onLoadItem(i.id)">{{ i.id }}</a>
+                </div>
+              </v-flex>
+
+              <v-flex xs12 md4>
+                <div><b>ID юзера</b></div>
+                <div v-for="i in items" :key="i.id" v-if="i.isModerated">
+                  <a @click="onLoadUser(i.creatorId)">{{ i.creatorId }}</a>
+                </div>
+              </v-flex>
+
+              <v-flex xs12 md2>
+                <div><b>Победитель</b></div>
+                <div v-for="i in items" :key="i.id" v-if="i.isModerated">
 
                   <span>
 
